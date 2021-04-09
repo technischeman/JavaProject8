638ad7d59fd640e2ecadf185ccf1e6e5b7266c5c
technischeman <79165779+technischeman@users.noreply.github.com>
2021-04-09 23:25:06 +0200

    Update homework1.java

--- src/homework1.java
+++ src/homework1.java
@@ -1,14 +1,14 @@
 public class homework1 {
     public static void main(String[] args) {
         int[]array={1535564,464465,651561};
-        int toplam=0;
+        int sum=0;
         for (int i = 0; i <= array.length ; i++) {
-            toplam+=array[i];
+            sum+=array[i];
 
 
-        } System.out.println("toplam"+toplam);
-        int ort=toplam/ array.length;
-        System.out.println("ort"+ort);
+        } System.out.println("sum"+sum);
+        int average=sum/ array.length;
+        System.out.println("average"+average);
 
     }
 }
